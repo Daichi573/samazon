96de94dcbfe322d4514ef147f82212241aef25a6
Fukuda Daichi <djf576atn@gmail.com>
2021-07-10 03:07:01 +0000

    09 create_reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,13 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
+     @reviews = @product.reviews
+     @review = @reviews.new
   end
 
   def new
@@ -19,8 +22,6 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
-     @categories = Category.all
   end
 
   def update
@@ -30,11 +31,15 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_url
   end
   private
+  
+    def set_product
+      @product = Product.find(params[:id])
+    end
+    
     def product_params
       params.require(:product).permit(:name, :description, :price, :category_id)
     end
